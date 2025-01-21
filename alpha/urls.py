f8db5c70e59a775ba55c8c77058d80bcb7dda540
ramnath2310 <saran23ram@gmail.com>
2025-01-21 17:55:45 +0530

    added travello app &templates and static files

--- alpha/urls.py
+++ alpha/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('',include("calc.urls")),
+    # path('',include("calc.urls")),
+    path('',include("travello.urls")),
     path('admin/', admin.site.urls),
 ]
--- calc/views.py
+++ calc/views.py
@@ -4,4 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     # return HttpResponse("<h1>Hello world<h1>")
-    return render(request,'index.html',{'name':'Naveen'})
+    return render(request,'cal.html',{'name':'Naveen'})
+
+def add(request):
+
+    val1=int(request.POST["num1"])
+    val2=int(request.POST["num2"])
+    res=val1+val2
+
+    return render(request,"result.html",{"result":res})
