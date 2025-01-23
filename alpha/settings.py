e498130290f049848e9d2d00436d58dc1d9a83c8
ramnath2310 <saran23ram@gmail.com>
2025-01-23 17:57:58 +0530

    created models and fetched data from admin

--- alpha/settings.py
+++ alpha/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'alpha.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'telusko',
+        'USER': 'postgres',
+        'PASSWORD':'1234',
+        'HOST':'localhost'
+
     }
 }
 
@@ -123,6 +128,12 @@ STATICFILES_DIRS=[
 ]
 
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+
+
+# Adding image folder root path
+
+MEDIA_URL='/media/'
+MEDIA_ROOT =os.path.join(BASE_DIR,'media') 
 
 
 
--- travello/views.py
+++ travello/views.py
@@ -5,28 +5,30 @@ from .models import Destination
 
 def index(request):
 
-    dest1 = Destination()
-    dest1.name ='Chennai'
-    dest1.desc ='One of the hotest city in india'
-    dest1.img='destination_1.jpg'
-    dest1.offer =False
-    dest1.price = 700
+    # dest1 = Destination()
+    # dest1.name ='Chennai'
+    # dest1.desc ='One of the hotest city in india'
+    # dest1.img='destination_1.jpg'
+    # dest1.offer =False
+    # dest1.price = 700
 
-    dest2=Destination()
-    dest2.name = "Mumbai"
-    dest2.desc = "The city that never sleeps"
-    dest2.img='destination_3.jpg'
-    dest2.offer =False
-    dest2.price =800
+    # dest2=Destination()
+    # dest2.name = "Mumbai"
+    # dest2.desc = "The city that never sleeps"
+    # dest2.img='destination_3.jpg'
+    # dest2.offer =False
+    # dest2.price =800
 
-    dest3=Destination()
-    dest3.name ='Kerala'
-    dest3.desc ='It has beautiful nature'
-    dest3.img='destination_2.jpg'
-    dest3.offer =True
-    dest3.price =900
+    # dest3=Destination()
+    # dest3.name ='Kerala'
+    # dest3.desc ='It has beautiful nature'
+    # dest3.img='destination_2.jpg'
+    # dest3.offer =True
+    # dest3.price =900
 
-    dests=[dest1,dest3,dest2]
+    # dests=[dest1,dest3,dest2]
+
+    dests=Destination.objects.all()
 
     return render(request,"index.html",{'dests':dests})
     
